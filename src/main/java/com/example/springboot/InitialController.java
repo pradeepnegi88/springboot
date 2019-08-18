fe91bfebd5d1cbc9ef69c761ab8fd3e7a9d07b84
PradeepNegi <pradeepnegi2012@gmail.com>
2019-08-18 11:44:34 +0530

    initial controller

---
+++ src/main/java/com/example/springboot/InitialController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/initial")
+public class InitialController {
+    @GetMapping
+    public String getInitial(){
+        return "This is the default controller response";
+    }
+}
